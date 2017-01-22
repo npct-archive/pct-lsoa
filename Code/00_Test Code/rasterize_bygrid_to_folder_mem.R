625eee20f618eb460471f5935a2888926cb0e101
Malcolm Morgan <malcolmmorgan02@hotmail.com>
2017-01-22 13:26:23 +0000

    fix sorting error
    fixed sorting of the tab file

--- Code/00_Test Code/rasterize_bygrid_to_folder_mem.R
+++ Code/00_Test Code/rasterize_bygrid_to_folder_mem.R
@@ -9,13 +9,13 @@ library(maptools)
 
 
 #Inputs
-routes_master = readRDS("../pct-lsoa/Data/03_Intermediate/routes/rf_nat_4plus_fix.Rds")   #CHANGE ME
+routes_master = readRDS("../pct-lsoa/Data/03_Intermediate/routes/rf_nat_less3p_fix.Rds")   #CHANGE ME
 flow_data = readRDS("../pct-lsoa/Data/02_Input/LSOA_flow.Rds") #CHANGE ME
 
 #Parameters
 resolution = 10 #Pixle size in meters
 #tempfld <- "../pct-lsoa/Data/03_Intermediate/temp/rasterise-par/" #Where tempry rasters are saved
-outfld <- "../pct-lsoa/Data/03_Intermediate/raster-grid-par/" #Where results are saved
+outfld <- "C:/results/census3l/" #Where results are saved
 
 #Make Out folder
 if (!isTRUE(file.info(outfld)$isdir)) dir.create(outfld, recursive=TRUE)
@@ -48,22 +48,16 @@ routes_master@data$grid <- over$id
 remove("over","bb","cs","cc","cd","grd","points","sp_grd","grid_size")
 
 tab <- as.data.frame(table(routes_master$grid))
-tab <- tab[order(-Freq),]
+names(tab) <- c("grid","count")
+tab$grid <- as.integer(as.character(tab$grid))
+tab$count <- as.integer(tab$count)
+tab <- tab[order(tab$count),]
 write.csv(tab,paste0(outfld,"run_order.csv"))
 print(paste0("There are ",nrow(tab), " grids to do"))
 
-
-#Do grids Parallel
-#library(doSNOW)
-#library(foreach)
-#cl<-makeCluster(1) #change the 2 to your number of CPU cores
-#registerDoSNOW(cl)
-
-#Loop though each grid cell and make a raster
-#foreach(i = as.integer(as.character(tab$Var1))) %dopar% {
-for(i in tab$Var1){
+for(i in tab$grid){
   
-  print(paste0("Doing ",tab$Freq[tab$Var1==i]," lines in  grid number ", i," at ",Sys.time()))
+  print(paste0("Doing ",tab$count[tab$grid==i]," lines in  grid number ", i," at ",Sys.time()))
   lines <- routes_master[routes_master$grid == i,]
   names(lines) <- c("id","bike","grid")
   polys <- gBuffer(lines, byid = T, width = 10)
@@ -74,14 +68,14 @@ for(i in tab$Var1){
   dataType(raster_master) <- "INT2U"
   vx <- velox(raster_master, extent=extent(polys), res=c(resolution,resolution), crs="+init=epsg:27700")
   remove(raster_master)
-  print(paste0("SetUp",Sys.time()))
+  #print(paste0("SetUp",Sys.time()))
   
     #Make and empty raster stack
     lx <- list(vx)
     for (j in 1:nrow(polys)) lx[[j]] <- vx
     vx_sub2 <- velox(lx, extent=extent(polys), res=c(resolution,resolution), crs="+init=epsg:27700")
     remove(lx, vx)
-    print(paste0("Make ",Sys.time()))
+    #print(paste0("Make ",Sys.time()))
     
     #loop though each line and rasterize
     for(k in 1:nrow(polys) ){  
@@ -89,20 +83,20 @@ for(i in tab$Var1){
       vx_sub2$rasterize(lines2raster, field="bike", band= k, background = 0)
     }
     remove(lines2raster, polys)
-    print(paste0("Rastered ",Sys.time()))
+    #print(paste0("Rastered ",Sys.time()))
     
     #Add rasters togther
     rs <- vx_sub2$as.RasterStack()
     remove(vx_sub2)
     rsum <- stackApply(rs, 1, sum)
-    print(paste0("Added ",Sys.time()))
+    #print(paste0("Added ",Sys.time()))
     
     writeRaster(rsum,filename = paste0(outfld,"grd-",i,".tif"), format ="GTiff")
-    print(paste0("Written ",Sys.time()))
+    #print(paste0("Written ",Sys.time()))
     
     removeTmpFiles(h = 1)
     remove(rs,rsum)
-    print(paste0("Cleaned ",Sys.time()))
+    #print(paste0("Cleaned ",Sys.time()))
 }
 
 
