6da3814247ca60b9ce2fe0a12091f112272ec576
Malcolm <malcolmmorgan02@hotmail.com>
2017-01-22 23:23:07 +0000

    more rasterisation code tweaks

---
+++ Code/00_Test Code/argis_export2.R
@@ -0,0 +1,58 @@
+#Convert routes to a raster based on the number of cyclists
+#WHen rasterizing it is necessary to not have overlapping lines
+
+#4) Loop though each group convert to raster then add up rasters
+
+#libs
+library(sp)
+library(rgeos)
+library(velox)
+library(raster)
+library(dplyr)
+library(rgdal)
+
+#Inputs
+routes_master = readRDS("E:/OneDrive - University of Leeds/Cycling Big Data/LSOA/rf_nat_4plus_fix.Rds")
+#groups_master = readRDS("../pct-lsoa/Data/03_Intermediate/groups/rf_nat_4plus_region2_finished.Rds")
+flow_data = readRDS("E:/OneDrive - University of Leeds/Cycling Big Data/LSOA/LSOA_flow.Rds")
+
+#Parameters
+resolution = 20 #Equal to OSM zoom level 13
+
+#Prep and Join data
+#groups_master$id_old = as.character(groups_master$id_old)
+#groups_master = groups_master[!is.na(groups_master$group),]
+flow_data = flow_data[,c("id","bicycle_16p")]
+#flow_data = flow_data[flow_data$id %in% groups_master$id_old,]
+#routes_master = routes_master[routes_master$ID %in% groups_master$id_old,]
+#flow_data = left_join(flow_data,groups_master, by = c("id" = "id_old"))
+routes_master@data = left_join(routes_master@data, flow_data, by = c("ID" = "id"))
+
+#Remove Unneeded data
+#routes_master@data = routes_master@data[,c("ID","bicycle_16p","group")]
+routes_master@data = routes_master@data[,c("ID","bicycle_16p")] #,"group")]
+remove(flow_data)
+routes_master = routes_master[routes_master$bicycle_16p >0,]
+
+#group_list = as.data.frame(table(routes_master$group))
+#group_list$Var1 = as.integer(group_list$Var1)
+writeOGR(routes_master,dsn = "E:/OneDrive - University of Leeds/Cycling Big Data/LSOA/shape", layer = "rf_nat_4plus_fix", driver = "ESRI Shapefile")
+
+
+routes_master <- spTransform(routes_master, CRS( "+init=epsg:27700" ) )
+routes_poly <- gBuffer(routes_master, byid = T, width = 12) 
+raster_master <- raster(resolution = resolution, ext = extent(routes_master), crs= "+init=epsg:27700", vals = 0)
+dataType(raster_master) <- "INT2U"
+vx <- velox(raster_master, extent=extent(routes_master), res=c(resolution,resolution), crs="+init=epsg:27700")
+remove(raster_master)
+
+#loop though each group and rasterize
+print(paste0("There are ",nrow(group_list)," groups to do, getting started at ",Sys.time() ))
+for(k in group_list$Var1 ){  #max(groups[,2])
+  lines2raster = routes_poly[routes_poly$group == k ,]
+  vx_sub <- vx
+  vx_sub$rasterize(lines2raster, field="bicycle_16p", band=1, background = 0)
+  vx_sub$write(path = paste0("../pct-lsoa/Data/03_Intermediate/temp/rasters/rf-nat-4plus-region1-",k,".tif"))
+  print(paste0("Done group ",k," at ",Sys.time() ))
+}
+
---
+++ Code/00_Test Code/rasterize_mem_limit.R
@@ -0,0 +1,136 @@
+library(sp)
+library(rgeos)
+library(velox)
+library(raster)
+library(dplyr)
+library(rgdal)
+library(maptools)
+
+#Inputs
+routes_master = readRDS("../pct-lsoa/Data/03_Intermediate/routes/rf_nat_4plus_fix.Rds")   #CHANGE ME - input rf_nat_all.Rds
+flow_data = read.csv("E:/OneDrive - University of Leeds/Cycling Big Data/LSOA/flow_results_nat_round_170121.csv") #CHANGE ME - input flow_results_nat_round_170121.csv
+outfld <- "C:/results/test/" #CHANGE ME - where results are saved 
+
+#Parameters
+resolution = 10 #Pixle size in meters
+limit = 200 #Maximum number of lines to be done at once
+
+#Make Out folder
+if (!isTRUE(file.info(outfld)$isdir)) dir.create(outfld, recursive=TRUE)
+
+#Prep and Join data
+routes_master@data <- left_join(routes_master@data, flow_data, by = c("ID" = "id"))
+
+#Remove Unneeded data
+routes_master@data = routes_master@data[,c("ID","ebike_slc_r")]  #CHANGE ME #Change for different scenarios
+remove(flow_data)
+routes_master = routes_master[routes_master$ebike_slc_r >0,] #CHANGE ME #Change for different scenarios
+nrow(routes_master)
+routes_master <- spTransform(routes_master, CRS( "+init=epsg:27700" ) )
+points <- SpatialLinesMidPoints(routes_master)
+
+### define SpatialGrid object
+grid_size = 10000
+bb <- bbox(points)
+cs <- c(grid_size, grid_size)
+cc <- bb[, 1] + (cs/2)  # cell offset
+cd <- ceiling(diff(t(bb))/cs)  # number of cells per direction
+grd <- GridTopology(cellcentre.offset=cc, cellsize=cs, cells.dim=cd)
+sp_grd <- SpatialGridDataFrame(grd, data=data.frame(id=1:prod(cd)), proj4string=CRS(proj4string(points)))
+
+#Assign Grid IDs to Lines
+routes_master@data$grid <- as.integer(NA)
+over <- over(points, sp_grd)
+routes_master@data$grid <- over$id
+remove("over","bb","cs","cc","cd","grd","points","sp_grd","grid_size")
+
+tab <- as.data.frame(table(routes_master$grid))
+names(tab) <- c("grid","count")
+tab$grid <- as.integer(as.character(tab$grid))
+tab$count <- as.integer(tab$count)
+tab <- tab[order(tab$count),]
+write.csv(tab,paste0(outfld,"run_order.csv"))
+print(paste0("There are ",nrow(tab), " grids to do"))
+
+for(i in tab$grid){
+  #start <- Sys.time()
+  print(paste0("Doing grid ",i," with ",tab$count[tab$grid==i]," lines, at ",Sys.time()))
+  lines <- routes_master[routes_master$grid == i,]
+  names(lines) <- c("id","bike","grid")
+  polys <- gBuffer(lines, byid = T, width = 10)
+  remove(lines)
+  
+  #Set Up Raster
+  raster_master <- raster(resolution = resolution, ext = extent(polys), crs= "+init=epsg:27700", vals = 0)
+  dataType(raster_master) <- "INT2U"
+  vx <- velox(raster_master, extent=extent(polys), res=c(resolution,resolution), crs="+init=epsg:27700")
+  remove(raster_master)
+  #print(paste0("SetUp",Sys.time()))
+  
+  if(nrow(polys) <= limit){
+    #Make and empty raster stack
+    lx <- list(vx)
+    for (j in 1:nrow(polys)) lx[[j]] <- vx
+    vx_sub2 <- velox(lx, extent=extent(polys), res=c(resolution,resolution), crs="+init=epsg:27700")
+    remove(lx, vx)
+    #print(paste0("Make ",Sys.time()))
+    
+    #loop though each line and rasterize
+    for(k in 1:nrow(polys) ){  
+      lines2raster = polys[k,]
+      vx_sub2$rasterize(lines2raster, field="bike", band= k, background = 0)
+    }
+    remove(lines2raster, polys)
+    #print(paste0("Rastered ",Sys.time()))
+    
+    #Add rasters togther
+    rs <- vx_sub2$as.RasterStack()
+    remove(vx_sub2)
+    rsum <- stackApply(rs, 1, sum)
+    #print(paste0("Added ",Sys.time()))
+    
+    writeRaster(rsum,filename = paste0(outfld,"grd-",i,".tif"), format ="GTiff")
+    #print(paste0("Written ",Sys.time()))
+    
+    removeTmpFiles(h = 1)
+    remove(rs,rsum)
+    #print(paste0("Cleaned ",Sys.time()))
+    #end <- Sys.time()
+    #nlines <- tab$count[tab$grid==i]
+    #print(paste0("Done grid ",i," with ",nlines," lines, at ",(end-start)/(nlines)," s/line, at ",Sys.time()))
+  } else {
+    
+    print("Too large breaking into chunks")
+    for(l in 1:ceiling(nrow(polys)/limit)){
+      print
+      lstart <- 1 + limit * (l-1)
+      lfin <- if(limit * l > nrow(polys)){nrow(polys)}else{limit*l}
+      lx <- list(vx)
+      for (j in 1:limit) lx[[j]] <- vx
+      vx_sub2 <- velox(lx, extent=extent(polys), res=c(resolution,resolution), crs="+init=epsg:27700")
+      remove(lx, vx)
+      #print(paste0("Make ",Sys.time()))
+      
+      #loop though each line and rasterize
+      for(k in lstart:lfin ){  
+        lines2raster = polys[k,]
+        vx_sub2$rasterize(lines2raster, field="bike", band= (k - lstart + 1), background = 0)
+      }
+      remove(lines2raster, polys)
+      #print(paste0("Rastered ",Sys.time()))
+      #Add rasters togther
+      rs <- vx_sub2$as.RasterStack()
+      remove(vx_sub2)
+      rsum <- stackApply(rs, 1, sum)
+      #print(paste0("Added ",Sys.time()))
+      writeRaster(rsum,filename = paste0(outfld,"grd-",i,"-",k,".tif"), format ="GTiff")
+      #print(paste0("Written ",Sys.time()))
+      removeTmpFiles(h = 1)
+      remove(rs,rsum)
+      
+    }
+  }
+  
+}
+
+
